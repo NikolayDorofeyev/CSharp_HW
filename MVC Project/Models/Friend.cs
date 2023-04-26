aa803a6d2954ce3414440c5c1ed2e52f9e7310f5
dorofeyev.nikolay <dorofeyev.nikolay@gmail.com>
2023-04-26 20:04:32 +0300

    add: MVC Project

---
+++ MVC Project/Models/Friend.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project.Models
+{
+    public class Friend
+    {
+        [Required(ErrorMessage = "FriendID is required")]
+        public int FriendID { get; set; }
+
+        [Required(ErrorMessage = "Friend Name is required")]
+        [Display(Name = "Friend Name")]
+        [StringLength(25, ErrorMessage = "Friend Name should not exceed 25 characters")]
+        public string FriendName { get; set; }
+
+        [StringLength(25, ErrorMessage = "Place should not exceed 25 characters")]
+        public string Place { get; set; }
+    }
+}
